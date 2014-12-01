ea47ef468f5f9bb1835646b53d71457524662363
Untung Ginardi <untung@netzme.id>
2014-12-01 17:33:04 +0700

    salah masuk repo

--- learnNode/firsIO.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by untung on 19/09/14.
- */
-var fs = require('fs');
-var fileName = process.argv[2];
-var fileBuffer = fs.readFileSync(fileName);
-var strBuffer = fileBuffer.toString();
-var arrBuffer = strBuffer.split('\n');
-console.log(arrBuffer.length - 1);
